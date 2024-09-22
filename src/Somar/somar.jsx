2f73e300168fd78038b351a41de2eb10013cb556
jonascsaraiva <jonascsaraiva@outlook.com>
2024-09-22 11:07:41 -0300

    initial commit

---
+++ src/Somar/somar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+import "./somar.css"
+
+export default function Somar() {
+
+    const [inputA, setInputA] = useState(0);
+    const [inputB, setInputB] = useState(0);
+    const [resultado, setResultado] = useState(0);
+
+    const somar = () => {
+            // Verifica se os campos estão vazios
+        if (inputA === '' || inputB === '') {
+            setResultado("Por favor, preencha os dois campos.");
+        } else {
+            const numA = parseFloat(inputA);
+            const numB = parseFloat(inputB);
+            // Verifica se os valores digitados são números válidos
+            if (isNaN(numA) || isNaN(numB)) {
+                setResultado("Por favor, insira números válidos.");
+            } else {
+                setResultado(numA + numB);
+            }
+        }
+    };
+
+    return(
+        <div id="somarPai">
+            <h1> Somador </h1>
+
+            <section id="somarFilho">
+
+            <input 
+            type="number" 
+            placeholder="Digite um numero" 
+            value={inputA} 
+            onChange={ (e) => setInputA(e.target.value) }
+            />
+
+            <input 
+            type="number" 
+            placeholder="Digite outro numero" 
+            value={inputB} 
+            onChange={ (e) => setInputB(e.target.value) }
+            />
+
+            <button onClick={ (somar) }>Somar</button>
+
+            </section>
+
+            <p>{resultado}</p>
+        </div>
+    )
+}
---
+++ src/idade/calculadoraIdade.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export default function calculadoraIdade() {
+    
+
+
+    return(
+        <h1>Ola você ai</h1>
+    )
+}
